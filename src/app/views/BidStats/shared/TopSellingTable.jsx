7cd9f3126ab12022b931bed308d7511ae0000c1f
shubhamgosewade <shubham1835@gmail.com>
2024-04-15 23:04:14 +0530

    loader added

--- src/app/views/BidStats/shared/TopSellingTable.jsx
+++ src/app/views/BidStats/shared/TopSellingTable.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect } from 'react'
 import {
-  Avatar,
   Box,
   Card,
-  Icon,
-  IconButton,
-  MenuItem,
-  Select,
   styled,
   Table,
   TableBody,
@@ -18,21 +13,7 @@ import {
 import { Paragraph } from 'app/components/Typography';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAggregate } from 'app/redux/actions/BidAction';
-
-const CardHeader = styled(Box)(() => ({
-  display: 'flex',
-  paddingLeft: '24px',
-  paddingRight: '24px',
-  marginBottom: '12px',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-}));
-
-const Title = styled('span')(() => ({
-  fontSize: '1rem',
-  fontWeight: '500',
-  textTransform: 'capitalize',
-}));
+import { MatxLoading } from 'app/components'
 
 const ProductTable = styled(Table)(() => ({
   minWidth: 400,
@@ -47,37 +28,16 @@ const ProductTable = styled(Table)(() => ({
   '& td:first-of-type': { paddingLeft: '16px !important' },
 }));
 
-const Small = styled('small')(({ bgcolor }) => ({
-  width: 50,
-  height: 15,
-  color: '#fff',
-  padding: '2px 8px',
-  borderRadius: '4px',
-  overflow: 'hidden',
-  background: bgcolor,
-  boxShadow: '0 0 2px 0 rgba(0, 0, 0, 0.12), 0 2px 2px 0 rgba(0, 0, 0, 0.24)',
-}));
-
 const TopSellingTable = () => {
   const dispatch = useDispatch();
   const { palette } = useTheme();
-  const bgError = palette.error.main;
-  const bgPrimary = palette.primary.main;
-  const bgSecondary = palette.secondary.main;
   useEffect(() => {
     dispatch(getAggregate())
   }, []);
   const bidList = useSelector((state) => state.bidReducer.aggregateBids);
   return (
     <Card elevation={3} sx={{ pt: '20px', mb: 3 }}>
-      <CardHeader>
-        <Title>Dashboard</Title>
-        <Select size="small" defaultValue="this_month">
-          <MenuItem value="this_month">This Month</MenuItem>
-          <MenuItem value="last_month">Last Month</MenuItem>
-        </Select>
-      </CardHeader>
-
+      {!bidList && <MatxLoading></MatxLoading>}
       <Box overflow="auto">
         <ProductTable>
           <TableHead>
