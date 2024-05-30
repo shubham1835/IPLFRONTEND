0caf4a089750350d63e296c144b0d4b8dc6959be
shubhamgosewade <shubham1835@gmail.com>
2024-05-30 10:53:46 +0530

    firebase notification

--- src/app/contexts/SettingsContext.js
+++ src/app/contexts/SettingsContext.js
@@ -1,15 +1,45 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
 import { merge } from 'lodash'
 
 import { MatxLayoutSettings } from 'app/components/MatxLayout/settings'
-
+import { getToken } from "firebase/messaging";
+import { messaging } from '../../config';
+import { getMessaging, onMessage } from "firebase/messaging";
 const SettingsContext = createContext({
     settings: MatxLayoutSettings,
-    updateSettings: () => {},
+    updateSettings: () => { },
 })
 
 export const SettingsProvider = ({ settings, children }) => {
+    const fmessaging = getMessaging();
+    onMessage(fmessaging, (payload) => {
+        console.log('Message received. ', payload);
+        // ...
+    });
+    const VITE_APP_VAPID_KEY = 'BIQ_pfxscjdI7gMAovSKk88uEJoOoWyK_lZijsutlNXzg59YWRFoGnKotECF3-kukX1udV7LsnUWWbzwqF0yMzw';
+    async function requestPermission() {
+        //requesting permission using Notification API
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+            const token = await getToken(messaging, {
+                vapidKey: VITE_APP_VAPID_KEY,
+            });
+
+            //We can send token to server
+            console.log("Token generated : ", token);
+            window.localStorage.setItem('firebaseToken', token)
+        } else if (permission === "denied") {
+            //notifications are blocked
+            alert("You denied for the notification");
+        }
+    }
+
+    useEffect(() => {
+        requestPermission();
+    }, []);
+
     const [currentSettings, setCurrentSettings] = useState(
         settings || MatxLayoutSettings
     )
--- src/config.js
+++ src/config.js
@@ -14,5 +14,28 @@
 //     domain: 'matx.us.auth0.com',
 // }
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export var firebaseConfig = {
+    apiKey: "AIzaSyBy3ucvIJ9qkkFNWUbRhKMnyIDCzMW6fOg",
+    authDomain: "ipl-saatta.firebaseapp.com",
+    projectId: "ipl-saatta",
+    storageBucket: "ipl-saatta.appspot.com",
+    messagingSenderId: "296381669945",
+    appId: "1:296381669945:web:cdf7c091475140463fe3e8",
+    measurementId: "G-MNY9MELM1K"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Messaging service
+export const messaging = getMessaging(app);
+
 export const SERVER_URI = 'https://merchant-middleware-1-0-6.onrender.com';
 // export const SERVER_URI = 'http://127.0.0.1:8083';
